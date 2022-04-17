82a027fa622263ad22ba9ef2dc840154ac28ad0f
jonathan louie <jonathan.louie@hotmail.com>
2022-04-17 02:09:59 -0700

    RangeSumQueryImmutable class problem

---
+++ CodingPracticeService/ClassProblems/P303RangeSumQueryImmutable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPracticeService.ClassProblems
+{
+    class P303RangeSumQueryImmutable
+    {
+        /// 303. Range Sum Query - Immutable
+        /// time O(n) space O(1)
+        public P303RangeSumQueryImmutable(int[] nums)
+        {
+            Nums = nums;
+        }
+        public int[] Nums;
+        public int SumRange(int left, int right)
+        {
+            int sum = 0;
+            for (int i = left; i <= right; i++)
+            {
+                sum += Nums[i];
+            }
+            return sum;
+        }
+    }
+}
